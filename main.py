f0f35046b46b389d74241a815ac625e15668ae14
juan <juan.cantero.jarilla@gmail.com>
2019-07-17 17:24:52 +0200

    Segundo commit del proyecto

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask import Request
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hola mundo'
-    
-app.run()
+    xuxu = 'xuxi'
+    return render_template('index.html', xuxu = xuxu)
+
+if __name__== '__main__':
+    app.run(debug=True, port=8000)

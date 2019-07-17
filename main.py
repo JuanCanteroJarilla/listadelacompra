8d55fa2e2460f7b0fcd4ac8057948a78cc1b7ac0
juan <juan.cantero.jarilla@gmail.com>
2019-07-17 17:53:17 +0200

    Tercer commit del proyecto

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    xuxu = 'xuxi'
-    return render_template('index.html', xuxu = xuxu)
+    xuxu = ['xuxi','xuxu','xuxaki']
+    return render_template('index.html', list = xuxu)
 
 if __name__== '__main__':
     app.run(debug=True, port=8000)
